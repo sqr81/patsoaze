c0d4c23d1a7231faa5d0b2ad62a958f68ba183c4
sqr81 <sqr81@free.fr>
2021-03-07 11:57:08 +0100

    contact home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,12 +1,17 @@
 <?php
 namespace App\Controller;
 
+use App\Form\ContactHomeType;
 use App\Repository\ActualiteRepository;
 use App\Repository\AquarelleRepository;
 use App\Repository\PhotoRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 use Vich\UploaderBundle\Form\Type\VichImageType;
@@ -21,15 +26,43 @@ class HomeController extends AbstractController
         $this->em = $em;
 
     }
+
     /**
      * @Route("/", name="home")
      * @param AquarelleRepository $repository
      * @param PhotoRepository $photoRepository
      * @param ActualiteRepository $actualiteRepository
+     * @param Request $request
+     * @param MailerInterface $mailer
      * @return Response
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
-    public function index(AquarelleRepository $repository, PhotoRepository $photoRepository, ActualiteRepository $actualiteRepository): Response
+    public function index(AquarelleRepository $repository, PhotoRepository $photoRepository, ActualiteRepository $actualiteRepository, Request $request, MailerInterface $mailer): Response
     {
+        //formulaire de contact
+        $form = $this->createForm(ContactHomeType::class);
+        $contact = $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $email = (new TemplatedEmail())
+                ->from($contact->get('email')->getNormData())
+                ->to('sqr81@free.fr')
+                ->subject('Contact depuis Pat&Soaze')
+                ->htmlTemplate('emails/contact_home.html.twig')
+                ->context([
+                    'mail' => $contact->get('email')->getData(),
+                    'nom' => $contact->get('nom')->getData(),
+                    'message' => $contact->get('message')->getData(),
+                    'telephone' => $contact->get('telephone')->getData(),
+                ])
+            ;
+            $mailer->send($email);
+
+            $this->addFlash('message', 'Mail de contact envoyé');
+
+            return $this->redirectToRoute('home');
+
+        }
 
         $aquarelles = $repository->findLatest();
         $photos = $photoRepository->findLatest();
@@ -38,7 +71,15 @@ class HomeController extends AbstractController
             'aquarelles'=>$aquarelles,
             'photos'=>$photos,
             'actualites'=>$actualites,
-
+            'form' => $form->createView(),
         ]);
     }
+
+//    /**
+//     * @Route("/", name="home")
+//     */
+//    public function contact()
+//    {
+//        return $this->render();
+//    }
 }
---
+++ src/Form/ContactHomeType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use EasyCorp\Bundle\EasyAdminBundle\Form\Type\TextEditorType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactHomeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class,[
+                'label' => 'Votre e-mail',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Votre nom',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('message', TextEditorType::class, [
+                'label' => 'Votre message'
+            ])
+            ->add('telephone', TextEditorType::class, [
+                'label' => 'Votre téléphone'
+            ])
+            ->add('envoyer', SubmitType::class,[
+                'attr'=> [
+                    'class' => 'btn-primary'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
