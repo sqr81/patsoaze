f9c2c84e1587c173d6c3bb65e23e2411208c2a38
Yann <yann.sanquer@epitech.fr>
2021-12-15 10:26:28 +0100

    Photos artistes home

---
+++ migrations/Version20211110101758.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211110101758 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE exposition ADD created_at DATETIME DEFAULT NULL, DROP date');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE exposition ADD date DATE DEFAULT NULL, DROP created_at');
+    }
+}
